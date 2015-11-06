117bbabd447a11efb85a935c7a17ae00e48e1e0f
thiagolucas <tiagojp.lucas@gmail.com>
2015-11-06 13:36:01 -0200

    Update filtro.js
    atualizando filtro/bd

--- js/filtro.js
+++ js/filtro.js
@@ -2,19 +2,9 @@ angular.module('myApp', []).controller('ListaController', function($scope) {
 
           $scope.lojas = [
               {id:1, atividade:'Entretenimento', nome:'Y Designs Retro', bairro:'Pindorama', link: 'entretenimento/y/'},
-              {id:2, atividade:'Cosméticos',nome:'Hinode', bairro:'Pindorama', link: '#'},
-              {id:3, atividade:'Moda',nome:'Calçados, Roupas, Acessórios', bairro:'Pindorama', link: '#'},
-              {id:4, atividade:'Utilidades',nome:'Importados', bairro:'Glória', link: '#'},
-              {id:5, atividade:'Alimentação',nome:'Restaurante', bairro:'Glória', link: '#'},
-              {id:6, atividade:'Saúde',nome:'Clínica', bairro:'Glória', link: '#'},
-              {id:7, atividade:'Escolas',nome:'Particular', bairro:'Ipanema', link: '#'},
-              {id:8, atividade:'Igrejas',nome:'Ministério', bairro:'Ipanema', link: '#'},
-              {id:9, atividade:'Casa e Decoração',nome:'Lar', bairro:'Ipanema', link: '#'},
-              {id:10, atividade:'Construção',nome:'Depósito', bairro:'Coqueiros', link: '#'},
-              {id:11, atividade:'Vaículos',nome:'Concessionária', bairro:'Coqueiros', link: '#'},
-              {id:12, atividade:'Prestação de Serviços',nome:'Autônomo', bairro:'Coqueiros', link: '#'}
+              {id:2, atividade:'Cosméticos',nome:'Loide Hinode', bairro:'Pindorama', link: 'cosmeticos/loide/'},
+              {id:3, atividade:'Igreja',nome:'IPREV', bairro:'Pindorama', link: 'igrejas/iprev/'},
+              {id:3, atividade:'Serviço',nome:'Ludyana - Manicure/Pedicure', bairro:'Coqueiros', link: 'servicos/ludyana/'}
           ];
-
-
 
       });
